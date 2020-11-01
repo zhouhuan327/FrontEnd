ad0112fd1f1dba1fea9045126d35ea109c921031
zhouhuan <zsean21598@163.com>
2020-11-01 11:24:26 +0800

    深拷贝

---
+++ src/interview/手写/深拷贝.js
@@ -0,0 +1,84 @@
+function deepClone(target,cache = new Map()) {
+    if(!(target instanceof Object)) {
+        return target
+    }
+    if(cache.has(target)) {
+        return target
+    }
+    // 判断四种特殊情况
+    let dist;
+    if(target instanceof Array) {
+        dist = []
+    } else if(target instanceof Function) {
+        dist = function () {
+            return target.call(this,...arguments)
+        }
+    } else if(target instanceof Date) {
+        dist = new Date(target)
+    } else if(target instanceof RegExp) {
+        dist = new RegExp(target.source,target.flags)
+    } else {
+        dist = {}
+    }
+    cache.set(dist,target)
+    for(let key in target) {
+        if(target.hasOwnProperty(key)) {
+            dist[key] = deepClone(target[key],cache)
+        }
+    }
+    return dist
+}
+const a = {
+    i: Infinity,
+    s: "",
+    bool: false,
+    n: null,
+    u: undefined,
+    sym: Symbol(),
+    obj: {
+        i: Infinity,
+        s: "",
+        bool: false,
+        n: null,
+        u: undefined,
+        sym: Symbol(),
+    },
+    array: [
+        {
+            nan: NaN,
+            i: Infinity,
+            s: "",
+            bool: false,
+            n: null,
+            u: undefined,
+            sym: Symbol(),
+        },
+        123,
+    ],
+    fn: function () {
+        return "fn";
+    },
+    date: new Date(),
+    re: /hi\d/gi,
+};
+let a2 = deepClone(a);
+console.log(a2 !== a);
+console.log(a2.i === a.i);
+console.log(a2.s === a.s);
+console.log(a2.bool === a.bool);
+console.log(a2.n === a.n);
+console.log(a2.u === a.u);
+console.log(a2.sym === a.sym);
+console.log(a2.obj !== a.obj);
+console.log(a2.array !== a.array);
+console.log(a2.array[0] !== a.array[0]);
+console.log(a2.array[0].i === a.array[0].i);
+console.log(a2.array[0].s === a.array[0].s);
+console.log(a2.array[0].bool === a.array[0].bool);
+console.log(a2.array[0].n === a.array[0].n);
+console.log(a2.array[0].u === a.array[0].u);
+console.log(a2.array[0].sym === a.array[0].sym);
+console.log(a2.array[1] === a.array[1]);
+console.log(a2.fn !== a.fn);
+console.log(a2.date !== a.date);
+console.log(a2.re !== a.re);
