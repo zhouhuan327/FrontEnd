225aee7ced2d6b95303c581d601b4c686014f270
zhouhuan <zsean21598@163.com>
2020-11-21 15:55:06 +0800

    quick sort

---
+++ src/interview/ds/排序/QuickSort.js
@@ -0,0 +1,42 @@
+// 快速排序
+function quickSort(arr) {
+  const swap = (arr, a, b) => {
+    const temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  };
+  const handlePivot = (arr, start, end) => {
+    if (end - start <= 0) return -1;
+    if (end - start == 1) return 0;
+    // 随机找一个基准值，与start交换位置
+    const ramdomIndex = parseInt(Math.random() * (end - start) + start);
+    swap(arr[start], arr[ramdomIndex]);
+    let pivot = arr[start];
+    let smallEnd = start;
+    let bigStart = end;
+    let i = start + 1;
+    while (bigStart - smallEnd > 1) {
+      if (arr[i] >= pivot) {
+        bigStart--;
+        swap(arr, i, bigStart);
+        // 把大于基准的数弄到大数组的最后
+      } else {
+        smallEnd++;
+        swap(arr, i, smallEnd);
+        i++;
+      }
+    }
+    //交换基准和小数组的最后一位n这样基准的左边都比他小，右边都比他大
+    swap(arr, start, smallEnd);
+    return smallEnd;
+  };
+  const _quickSort = (arr, start, end) => {
+    if (end - start <= 1) return arr;
+    const pivot = handlePivot(arr, start, end);
+    _quickSort(arr, start, pivot);
+    _quickSort(arr, pivot + 1, end);
+    return arr;
+  };
+  return _quickSort(arr, 0, arr.length);
+}
+console.log(quickSort([1, 4, 2, 6, 7, 9, 33, 23]));
